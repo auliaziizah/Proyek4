2f4e19d10ce391fb124d174b6515aa6c6f989955
Aulia Aziizah Fauziyyah <auliaziizah@gmail.com>
2024-02-24 23:16:46 +0700

    Aulia Aziizah Fauziyyah Tugas 3 (Individu))

---
+++ mdc_100_series/lib/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const kShrinePink50 = Color.fromARGB(255, 139, 139, 139);
+const kShrinePink100 = Color.fromARGB(255, 226, 226, 226);
+const kShrinePink300 = Color.fromARGB(255, 44, 44, 44);
+const kShrinePink400 = Color.fromARGB(255, 0, 0, 0);
+
+const kShrineBrown900 = Color.fromARGB(255, 56, 56, 56);
+
+const kShrineErrorRed = Color(0xFFC5032B);
+
+const kShrineSurfaceWhite = Color.fromARGB(255, 255, 255, 255);
+const kShrineBackgroundWhite = Color.fromARGB(255, 255, 255, 255);
